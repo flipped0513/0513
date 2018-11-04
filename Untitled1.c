fb3f87ab3dbdb6381dc55f8ddad4e3d88b7d7855
flipped0513 <43701372+flipped0513@users.noreply.github.com>
2018-11-04 15:43:33 +0800

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,p,s;
+    s=0;
+    for(i=1;i<=4;i++)
+    {
+        p=1;
+        for(j=1;j<=4;j++)
+            p=p*j;
+        s=s+p;
+
+    }
+    printf("s=%d\n",s);
+}
